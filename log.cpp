7bbb1b05f860562261ca0abd9590cc6c3887a25a
orangefour <orange@four>
2017-08-25 15:55:10 +0200

    smaller log

--- log.cpp
+++ log.cpp
@@ -37,7 +37,7 @@ std::shared_ptr<spdlog::logger> Log::file() {
   static std::shared_ptr<spdlog::logger> logger;
   if (!logger) {
     logger = spdlog::rotating_logger_mt("file", qPrintable(path + "log.txt"),
-                                        1024 * 1024 * 1, 0);
+                                        128 * 1024 * 1, 0);
     logger->info("================= logger created =================");
   }
   return logger;
